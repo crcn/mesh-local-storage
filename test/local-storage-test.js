3686b3405e6f191a2ef3eefe34978e9fd5b25904
Craig Condon <craig.condon@hellosign.com>
2015-03-26 16:49:30 -0700

    clean up

--- test/local-storage-test.js
+++ test/local-storage-test.js
@@ -31,96 +31,47 @@ describe(__filename + "#", function() {
     expect(setStub.firstCall.args[1].a).to.be(1);
   });
 
-  it("returns an error if collection is not specified", function(next) {
-    var db   = crudlet(localStorageDatabase());
-    var stub = sinon.stub(db.target.store, "set");
-    db.insert({ data: { name: "abba" }}, function(err) {
-      expect(err).not.to.be(void 0);
-      next();
-    });
-    stub.restore();
-  });
-
-  it("can specify collection in constructor", function(next) {
-    var db   = crudlet(localStorageDatabase({ collection: "words" }));
-    var stub = sinon.stub(db.target.store, "set");
-    db.insert({ data: { name: "abba" }}, function(err) {
-      stub.restore();
-      expect(err).to.be(void 0);
-      expect(stub.callCount).to.be(1);
-      expect(stub.firstCall.args[0]).to.be("crudlet-db");
-      next();
-    });
-  });
-
-  it("can insert an item", function() {
+  it("saves when inserting", function() {
     var db   = crudlet(localStorageDatabase({collection:"people"}));
-    var stub = sinon.stub(db.target.store, "set");
+    var setStub = sinon.stub(db.target.store, "set");
     db.insert({ data: { name: "abba" }});
-    expect(db.target.db.people[0].name).to.be("abba");
-    expect(stub.callCount).to.be(1);
-    stub.restore();
+    expect(setStub.callCount).to.be(1);
+    setStub.restore();
   });
 
-  it("can insert multiple items at once", function() {
-    var db   = crudlet(localStorageDatabase({collection:"people"}));
-    db.insert({ data: [{ name: "abba" }, {name:"baab"}]});
-    expect(db.target.db.people[0].name).to.be("abba");
-    expect(db.target.db.people[1].name).to.be("baab");
-  });
-
-  it("can update an item with a query", function() {
+  it("saves when updating", function() {
     var db   = crudlet(localStorageDatabase({collection:"people"}));
     db.insert({ data: { name: "abba" }});
-    db.insert({ data: { name: "abba" }});
-    db.update({ query: { name: "abba" }, data: { name: "baab" }});
-    expect(db.target.db.people[0].name).to.be("baab");
-    expect(db.target.db.people[1].name).to.be("abba");
+    var setStub = sinon.stub(db.target.store, "set");
+    db.insert({ query: { name: "abba" }, data: { name: "baab" }});
+    expect(setStub.callCount).to.be(1);
+    setStub.restore();
   });
 
-  it("can update multiple items with a query", function() {
+  it("saves when removing", function() {
     var db   = crudlet(localStorageDatabase({collection:"people"}));
     db.insert({ data: { name: "abba" }});
-    db.insert({ data: { name: "abba" }});
-    db.update({ multi: true, query: { name: "abba" }, data: { name: "baab" }});
-    expect(db.target.db.people[0].name).to.be("baab");
-    expect(db.target.db.people[1].name).to.be("baab");
-  });
-
-  it("can remove an item, and only one item", function() {
-    var db   = crudlet(localStorageDatabase({collection:"people"}));
-    db.insert({ data: { name: "abba" }});
-    db.insert({ data: { name: "abba" }});
-    expect(db.target.db.people.length).to.be(2);
-    db.remove({ query: { name: "abba" }});
+    var setStub = sinon.stub(db.target.store, "set");
     expect(db.target.db.people.length).to.be(1);
-  });
-
-  it("can remove multiple items", function() {
-    var db   = crudlet(localStorageDatabase({collection:"people"}));
-    db.insert({ data: { name: "abba" }});
-    db.insert({ data: { name: "abba" }});
-    expect(db.target.db.people.length).to.be(2);
-    db.remove({ multi: true, query: { name: "abba" }});
+    db.remove({ query: { name: "abba" }});
     expect(db.target.db.people.length).to.be(0);
+    expect(setStub.callCount).to.be(1);
+    setStub.restore();
   });
 
-  it("can load one item", function(next) {
-    var db   = crudlet(localStorageDatabase({collection:"people"}));
-    db.insert({ data: { name: "abba" }});
-    db.load({ query: { name: "abba" }}, function(err, item) {
-      expect(item.name).to.be("abba");
-      next();
-    });
-  });
-
-  it("can load multiple items", function(next) {
-    var db   = crudlet(localStorageDatabase({collection:"people"}));
-    db.insert({ data: [{ name: "abba" }, {name:"abba"}]});
-    db.load({ multi: true, query: { name: "abba" }}, function(err, items) {
-      expect(items[0].name).to.be("abba");
-      expect(items[1].name).to.be("abba");
-      next();
-    });
+  it("can specify a custom storage object", function() {
+    var i = 0;
+    var j = 0;
+    var db   = crudlet(localStorageDatabase({ collection: "words", store: {
+      get: function() {
+        i++;
+      },
+      set: function() {
+        j++
+      }
+    } }));
+    expect(i).to.be(1);
+    db.insert({data:{a:1}});
+    expect(j).to.be(1);
   });
 });

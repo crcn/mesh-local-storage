aff1a811b9cebc9de364a41329996bee262c37f6
Craig Condon <craig.condon@hellosign.com>
2015-03-26 16:23:39 -0700

    fix issues

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build Status](https://travis-ci.org/crcn/crudlet-local-storage.svg)](https://travis-ci.org/crcn/crudlet-local-storage) [![Coverage Status](https://coveralls.io/repos/crcn/crudlet-local-storage/badge.svg?branch=master)](https://coveralls.io/r/crcn/crudlet-local-storage?branch=master) [![Dependency Status](https://david-dm.org/crcn/crudlet-local-storage.svg)](https://david-dm.org/crcn/crudlet-local-storage) [![Gitter](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/crcn/crudlet-local-storage?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge)
+
 This module is a local storage database adapter for [crudlet](https://github.com/crcn/crudlet.js) - a library that makes it easy to persist data through multiple transports.
 
 #### Features
@@ -7,6 +9,73 @@ This module is a local storage database adapter for [crudlet](https://github.com
 - can be used with other database transports such as [crudlet-http](https://github.com/crcn/crudlet-http)
 - cascades operations to other transports
 
+
+#### installation
+
+```
+npm install crudlet-local-storage
+```
+
+```javascript
+var crudlet = require("crudlet");
+var localdb = require("crudlet-local-storage");
+
+var db = crudlet(localdb());
+```
+
+#### db localdb(options)
+
+creates a local crudelt database
+
+- `options` - options for the local db
+  - `storageKey` - storage key to use
+  - `store` - store to use
+
+```javascript
+var db = localdb({
+  storageKey: "storage-key",
+  store: {
+    get: function(storageKey) {
+      // return DB yere
+    },
+    set: function(storageKey, value) {
+      // set db here
+    }
+  }
+});
+```
+
+#### db.run(operation, options, onComplete)
+
+runs an operation
+
+- `operation` - operation to run can be: `insert`, `remove`, `update`, or `load`
+- `options` - operation specific options
+
+insert options:
+
+- `data` - data to insert. Can be an object, or an array to insert multiple
+
+```javascript
+db.run("insert", { data: [{ name: "a"}, { name: "b" }]}); // insert two items
+db.run("insert", { data: { name: "gg"}}); // insert one item
+```
+
+remove options:
+
+- `query` - mongodb search query
+- `multi` - TRUE if you want to remove multiple items (false by default)
+
+update options:
+
+- `query` - mongodb search query
+- `multi` - TRUE if you want to update multiple items (false by default)
+- `data` - data to set - this is merged with existing data
+
+load options:
+
+- `query` - mongodb search query
+- `multi` - TRUE if you want to load multiple items (one by default)
 
 
 
--- test/local-storage-test.js
+++ test/local-storage-test.js
@@ -23,17 +23,25 @@ describe(__filename + "#", function() {
     setStub.restore();
   });
 
+  it("can add local-storage specific options", function() {
+    var db   = crudlet(localStorageDatabase({ collection: "words", storageKey: "abba" }));
+    var setStub = sinon.stub(db.target, "insert");
+    db.run("insert", { localStorage: { a: 1}});
+    expect(setStub.callCount).to.be(1);
+    expect(setStub.firstCall.args[1].a).to.be(1);
+  });
+
   it("returns an error if collection is not specified", function(next) {
     var db   = crudlet(localStorageDatabase());
     var stub = sinon.stub(db.target.store, "set");
     db.insert({ data: { name: "abba" }}, function(err) {
       expect(err).not.to.be(void 0);
       next();
-    })
+    });
     stub.restore();
   });
 
-  it("can collection in constructor", function(next) {
+  it("can specify collection in constructor", function(next) {
     var db   = crudlet(localStorageDatabase({ collection: "words" }));
     var stub = sinon.stub(db.target.store, "set");
     db.insert({ data: { name: "abba" }}, function(err) {
@@ -45,12 +53,74 @@ describe(__filename + "#", function() {
     });
   });
 
-
   it("can insert an item", function() {
-    var db   = crudlet(localStorageDatabase());
+    var db   = crudlet(localStorageDatabase({collection:"people"}));
     var stub = sinon.stub(db.target.store, "set");
-    db.insert({ data: { name: "abba" }})
+    db.insert({ data: { name: "abba" }});
+    expect(db.target.db.people[0].name).to.be("abba");
+    expect(stub.callCount).to.be(1);
     stub.restore();
-  })
-});
+  });
 
+  it("can insert multiple items at once", function() {
+    var db   = crudlet(localStorageDatabase({collection:"people"}));
+    db.insert({ data: [{ name: "abba" }, {name:"baab"}]});
+    expect(db.target.db.people[0].name).to.be("abba");
+    expect(db.target.db.people[1].name).to.be("baab");
+  });
+
+  it("can update an item with a query", function() {
+    var db   = crudlet(localStorageDatabase({collection:"people"}));
+    db.insert({ data: { name: "abba" }});
+    db.insert({ data: { name: "abba" }});
+    db.update({ query: { name: "abba" }, data: { name: "baab" }});
+    expect(db.target.db.people[0].name).to.be("baab");
+    expect(db.target.db.people[1].name).to.be("abba");
+  });
+
+  it("can update multiple items with a query", function() {
+    var db   = crudlet(localStorageDatabase({collection:"people"}));
+    db.insert({ data: { name: "abba" }});
+    db.insert({ data: { name: "abba" }});
+    db.update({ multi: true, query: { name: "abba" }, data: { name: "baab" }});
+    expect(db.target.db.people[0].name).to.be("baab");
+    expect(db.target.db.people[1].name).to.be("baab");
+  });
+
+  it("can remove an item, and only one item", function() {
+    var db   = crudlet(localStorageDatabase({collection:"people"}));
+    db.insert({ data: { name: "abba" }});
+    db.insert({ data: { name: "abba" }});
+    expect(db.target.db.people.length).to.be(2);
+    db.remove({ query: { name: "abba" }});
+    expect(db.target.db.people.length).to.be(1);
+  });
+
+  it("can remove multiple items", function() {
+    var db   = crudlet(localStorageDatabase({collection:"people"}));
+    db.insert({ data: { name: "abba" }});
+    db.insert({ data: { name: "abba" }});
+    expect(db.target.db.people.length).to.be(2);
+    db.remove({ multi: true, query: { name: "abba" }});
+    expect(db.target.db.people.length).to.be(0);
+  });
+
+  it("can load one item", function(next) {
+    var db   = crudlet(localStorageDatabase({collection:"people"}));
+    db.insert({ data: { name: "abba" }});
+    db.load({ query: { name: "abba" }}, function(err, item) {
+      expect(item.name).to.be("abba");
+      next();
+    });
+  });
+
+  it("can load multiple items", function(next) {
+    var db   = crudlet(localStorageDatabase({collection:"people"}));
+    db.insert({ data: [{ name: "abba" }, {name:"abba"}]});
+    db.load({ multi: true, query: { name: "abba" }}, function(err, items) {
+      expect(items[0].name).to.be("abba");
+      expect(items[1].name).to.be("abba");
+      next();
+    });
+  });
+});
